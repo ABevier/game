33d2603901c14197d5914a5446a395b6d6c8ae98
Alan Bevier <awbevier@gmail.com>
2019-09-08 11:39:47 -0400

    i broke it

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,1 +1,13 @@
+import io from 'socket.io-client';
+
+const socket = io();
+
 console.log("HELLLLLLLLO WOOOOOOORLD!");
+
+socket.on("message", (data: string) => {
+    console.log(data);
+});
+
+function sendMsg() {
+    socket.emit("message", "HELLO WORLD");
+}
