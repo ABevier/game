b667fda6d2064260e644e823a4447077bc560e5b
Alan Bevier <awbevier@gmail.com>
2019-09-07 21:10:52 -0400

    hax

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import http from 'http';
+import io from 'socket.io';
 
 const app = express();
 const port = 8080;
 
+const server = new http.Server(app);
+
+app.use('/js', express.static(__dirname + 'public/js'));
+
 app.get('/', (req, res) => {
-    res.send('HELLO WORLD!');
+    res.sendFile(__dirname + '/public/html/index.html')
 });
+
+io.listen(server);
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
