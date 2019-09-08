f358513d4bf9dd662e61ee610fd0ca9788b5205f
Alan Bevier <awbevier@gmail.com>
2019-09-08 15:16:23 -0400

    well webpack doesn't fail

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+const config: Configuration = {
+  entry: ['./src/client/client'],
+  output: {
+    path: path.join(__dirname, 'dist', 'statics'),
+    filename: `[name]-[hash:8]-bundle.js`,
+    publicPath: '/statics/',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+        exclude: [/node_modules/, nodeModulesPath],
+      },
+      {
+        test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/,
+        use: 'url-loader?limit=10000',
+      },
+    ],
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
+};
+
+export default config;
