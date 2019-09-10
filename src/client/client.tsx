1ea7345cb1cd21858289eadf1f32f4bae0b50b4f
Alan Bevier <awbevier@gmail.com>
2019-09-09 23:28:11 -0400

    hacking

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './client.css';
 
-class Square extends React.Component {
+class Square extends React.Component<any, any> {
     render() {
         return (
             <button className="square">
-                {/* TODO */}
+                {this.props.value}
             </button>
         );
     }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i} />;
     }
 
     render() {
