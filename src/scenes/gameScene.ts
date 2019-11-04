391b0ae2becb30f6defa6546ce6a2e1b6588b856
Alan Bevier <awbevier@gmail.com>
2019-11-03 21:56:29 -0500

    hacking

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -2,8 +2,6 @@ import { sum } from "../engine/foo";
 
 export class GameScene extends Phaser.Scene {
 
-    private square: Phaser.GameObjects.Rectangle & {body: Phaser.Physics.Arcade.Body}
-
     constructor() {
         super({
             key: "GameScene",
@@ -16,8 +14,29 @@ export class GameScene extends Phaser.Scene {
         const result = sum(1, 2, 3);
         console.log(`The result is: ${result}`);
 
-        this.square = this.add.rectangle(400, 400, 100, 100, 0xFFFFFF) as any;
-        this.physics.add.existing(this.square);
+        this.makeContainer(100, 400);
+
+        this.makeContainer(375, 400);
+
+        this.makeContainer(650, 400);
+    }
+
+    private makeContainer(x: number, y: number): Phaser.GameObjects.Container {
+        const container = this.add.container(x, y);
+
+        const bground = this.add.rectangle(0, 0, 250, 100, 0x0000FF);
+        bground.setOrigin(0, 0);
+        container.add(bground);
+
+        const txt = this.add.text(5, 5, "Hero");
+        txt.setOrigin(0, 0);
+        container.add(txt);
+
+        const hp = this.add.text(5, 20, "HP: 100/100");
+        hp.setOrigin(0, 0);
+        container.add(hp);
+
+        return container;
     }
 
     public update() {
