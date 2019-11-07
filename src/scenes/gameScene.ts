88dd1acbf697fb9a178b668983ac048654013918
Alan Bevier <awbevier@gmail.com>
2019-11-06 21:52:02 -0500

    update

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,4 +1,5 @@
 import { sum } from "../engine/foo";
+import { StateManager, MainState } from "./battle/states";
 
 export class GameScene extends Phaser.Scene {
 
@@ -10,42 +11,56 @@ export class GameScene extends Phaser.Scene {
         })
     }
 
+    private stateManager: StateManager = new StateManager();
+    private characters: DCharacter[] = [];
+
     public create() {
         const result = sum(1, 2, 3);
         console.log(`The result is: ${result}`);
 
-        this.makeContainer("Hero1", 100, 400, false);
-        this.makeContainer("Hero2", 375, 400, false);
-        this.makeContainer("Hero3", 650, 400, false);
+        this.characters.push(new DCharacter(this, "Hero1", 100, 400, false));
+        this.characters.push(new DCharacter(this, "Hero2", 375, 400, false));
+        this.characters.push(new DCharacter(this, "Hero3", 650, 400, false));
 
-        this.makeContainer("Goblin1", 250, 100, true);
-        this.makeContainer("Goblin2", 525, 100, true);
+        this.characters.push(new DCharacter(this, "Goblin1", 250, 100, true));
+        this.characters.push(new DCharacter(this, "Goblin2", 525, 100, true));
+
+        this.stateManager.nextState(new MainState(this.stateManager, this.characters));
     }
 
-    private makeContainer(name: string, x: number, y: number, badGuy: boolean): Phaser.GameObjects.Container {
-        const container = this.add.container(x, y);
-
-        const color = badGuy ? 0xFF0000 : 0x0000FF;
-
-        const bground = this.add.rectangle(0, 0, 250, 100, color);
-        bground.setOrigin(0, 0);
-        bground.setInteractive();
-        bground.on('pointerdown', () => console.log(`Clicked on ${name}`));
-        
-        container.add(bground);
-
-        const txt = this.add.text(5, 5, name);
-        txt.setOrigin(0, 0);
-        container.add(txt);
-
-        const hp = this.add.text(5, 20, "HP: 100/100");
-        hp.setOrigin(0, 0);
-        container.add(hp);
-
-        return container;
-    }
 
     public update() {
         //TODO
+    }
+}
+
+export class DCharacter {
+    private readonly background: Phaser.GameObjects.Rectangle;
+
+    public constructor(scene: Phaser.Scene, name: string, x: number, y: number, badGuy: boolean) {
+        const container = scene.add.container(x, y);
+
+        const color = badGuy ? 0xFF0000 : 0x0000FF;
+
+        this.background = scene.add.rectangle(0, 0, 250, 100, color);
+        this.background.setOrigin(0, 0);
+        this.background.setInteractive();
+        this.background.on('pointerdown', () => {
+            console.log(`Clicked on ${name}`);
+            this.activate();
+        });
+        container.add(this.background);
+
+        const txt = scene.add.text(5, 5, name);
+        txt.setOrigin(0, 0);
+        container.add(txt);
+
+        const hp = scene.add.text(5, 20, "HP: 100/100");
+        hp.setOrigin(0, 0);
+        container.add(hp);
+    }
+
+    public activate(): void {
+        this.background.fillColor = 0x00FF00;
     }
 }
