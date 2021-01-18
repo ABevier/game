98bc699b6e9095026e76b534fba0bbc433ff41ad
Alan Bevier <awbevier@gmail.com>
2021-01-17 20:32:28 -0500

    refactor some

---
+++ src/v2/coords.ts
@@ -0,0 +1,20 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface OffsetCoord {
+  x: number;
+  y: number;
+}
+
+export interface Pixel {
+  x: number;
+  y: number;
+}
+
+export interface CubeCoord {
+  x: number;
+  y: number;
+  z: number;
+}
